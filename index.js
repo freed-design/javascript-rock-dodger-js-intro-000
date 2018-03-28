eb37a14b353dd7a9759b3bee84601db579bb70b9
Daniel Dunbar <danielqdunbar@gmail.com>
2018-03-28 22:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -150,9 +150,13 @@ function moveDodger(e) {
     document.addEventListener('keydown', function(e) {
       if (e.which === LEFT_ARROW) {
         moveDodgerLeft()
+        e.preventDefault()
+        e.stopPropagation()
         }
       if (e.which === RIGHT_ARROW) {
         moveDodgerRight()
+        e.preventDefault()
+        e.stopPropagation()
         }
     })
 }
